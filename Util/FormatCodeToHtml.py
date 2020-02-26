59e6ba7be65e4a90a8a76641c72ae17fc6241a8b
Hackerry <harrynjc@yahoo.com>
2020-02-26 00:33:01 -0800

    Finished post

--- Util/FormatCodeToHtml.py
+++ Util/FormatCodeToHtml.py
@@ -1,13 +1,18 @@
-code = '''// Method 3: Lambda Expression
-        Collections.sort(nums, (a,b) -> a % 2 == 0 ? (b % 2 == 0 ? a-b: 1): (b % 2 == 0 ? -1: a-b));
-        System.out.println(nums);
+code = \
+'''\
+String alphabet = "qwertyuiopasdfghjklzxcvbnm";
+StringBuilder stringBuilder = Arrays.stream((alphabet.split(""))).sorted()
+    .collect(() -> new StringBuilder(), 
+            (sb,s) -> sb.append(s), 
+            (sb1, sb2) -> sb1.append(sb2.toString()));
+System.out.println(stringBuilder.toString());
 '''
 
 code = code.replace('&','&amp;')
 code = code.replace('<','&lt;')
 code = code.replace('>','&gt;')
 
-result = '<ul>\n<li><pre>'
+result = "<ul class='ccode'>\n<li><pre>"
 for i in code:
     if i == '\n':
         result = result + '</pre></li>\n<li><pre>'
